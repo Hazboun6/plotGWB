afa8228db42c94fe180b0c393b93e380eb8dd97e
elinore <elinore.roebber@gmail.com>
2020-08-24 13:57:43 +0100

    setting up initial package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='plotGWB',
+      version='0.0',
+      author='Elinore Roebber',
+      packages=['plotGWB'],
+      python_requires='>=3',
+      install_requires=['numpy', 'scipy', 'pandas', 'healpy', 'matplotlib', 'seaborn']
+      )
